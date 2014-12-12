4aa452b1b92990bd5573381ed639dbeddad6f62e
Benoît Brochier <b.brochier@c2is.fr>
2014-12-12 22:17:17 +0100

    navigation system + localstorage state

--- js/main.js
+++ js/main.js
@@ -3,7 +3,29 @@
 
   $(function () {
 
-    /* Navigation
+    /* Set Nav state from localStorage
+       ========================================================================== */
+    if(localStorage && localStorage.getItem('styleguide-nav-state')){
+        var sgnav = localStorage.getItem('styleguide-nav-state');
+        if(sgnav == 1){
+          $('.sg-body').addClass('nav-is-opened');
+        }
+    }
+
+    /* Nav show/hide
+       ========================================================================== */
+    $('.sg-nav-trigger').on('click', function(){
+      $('.sg-body').toggleClass('nav-is-opened');
+      //set localstorage
+      if($('.sg-body').hasClass('nav-is-opened')){
+        localStorage.setItem('styleguide-nav-state', 1);
+      } else {
+        localStorage.setItem('styleguide-nav-state', 0);
+      }
+    })
+
+
+    /* Nav accordion
        ========================================================================== */
     $('.sg-nav span').on('click', function(){
       $(this).toggleClass('is-opened').next().slideToggle();
